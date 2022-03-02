566985e452420e8326052bcbf8425b1209ec154b
kasparmartens <kaspar.martens@gmail.com>
2022-03-02 23:08:23 +0000

    add Phenograph and UMAP with Seurat

--- R/helpers.R
+++ R/helpers.R
@@ -1,6 +1,6 @@
 `%not in%` <- purrr::negate(`%in%`)
 
-plot_rarity_unique_heatmap <- function(rarity, n_cells, scale_with_number_of_cells=FALSE, subset_clusters = NULL, subset_features = NULL, cluster_columns = TRUE, ...){
+plot_binary_signatures <- function(rarity, n_cells = 30, subset_clusters = NULL, subset_features = NULL, cluster_columns = TRUE, ...){
   if(is.null(subset_clusters)){
     counts <- rarity$counts
     unique_profiles <- rarity$unique_profiles
@@ -15,17 +15,11 @@ plot_rarity_unique_heatmap <- function(rarity, n_cells, scale_with_number_of_cel
   
   how_many <- sum(counts > n_cells)
   
-  if(scale_with_number_of_cells){
-    do.call("rbind", lapply(1:how_many, function(i){
-      do.call("rbind", lapply(1:counts[i], function(x){
-        unique_profiles[i, ]
-      }))
-    }))
-  } else{
-    mat <- do.call("rbind", lapply(1:how_many, function(i){
-      unique_profiles[i, ]
-    }))
-  }
+  mat <- do.call("rbind", lapply(1:how_many, function(i){
+    unique_profiles[i, ]
+  }))
+  
+  rownames(mat) <- sprintf("%5d cells", counts[1:how_many])
 
   ComplexHeatmap::Heatmap(
     mat,
@@ -42,6 +36,44 @@ plot_rarity_unique_heatmap <- function(rarity, n_cells, scale_with_number_of_cel
   )
 }
 
+plot_heatmap_with_observations <- function(Y, rarity, n_cells = 30, subset_cells = NULL, cluster_columns = TRUE, ...){
+  cluster_allocations <- rarity$cluster_allocations
+  n_clusters <- max(cluster_allocations)
+  tiny_clusters <- which(rarity$counts < n_cells)
+  if(is.null(subset_cells)){
+    subset_cells <- (cluster_allocations %not in% tiny_clusters)
+  } else{
+    subset_cells <- subset_cells & (cluster_allocations %not in% tiny_clusters)
+  }
+  
+  ComplexHeatmap::Heatmap(
+    Y[subset_cells, ],
+    col = c(RColorBrewer::brewer.pal(9, "Reds"), "#67000D"),
+    cluster_columns = cluster_columns,
+    row_split = cluster_allocations[subset_cells], 
+    cluster_row_slices = FALSE,
+    cluster_rows = TRUE,
+    use_raster = TRUE,
+    heatmap_legend_param = list(title = ""),
+    show_row_dend = FALSE,
+    show_column_dend = FALSE, 
+    ...
+  )
+}
+
+boxplot_diffexp_selected_clusters <- function(mat, cl){
+  df <- mat %>% 
+    data.frame() %>% 
+    mutate(cluster = factor(cl)) %>% 
+    gather(marker, value, -cluster)
+  
+  df %>% 
+    ggplot(aes(cluster, value, col=cluster)) +
+    geom_boxplot(outlier.shape = NA) +
+    facet_wrap(~ marker) +
+    theme_bw() +
+    theme(legend.position = "none")
+}
 
 mylog <- function(p) ifelse(p==0, 0, log(p))
 
@@ -80,5 +112,24 @@ helper_metrics_conditional <- function(freq_table, selected_cell_type){
     completeness = completeness,
     v_measure = 2 * homogeneity * completeness / (homogeneity + completeness)
   )
+}
+
+
+Seurat_UMAP <- function(Y){
+  rownames(Y) <- 1:nrow(Y)
+  
+  seurat_object <- Seurat::CreateSeuratObject(t(Y))
+  seurat_object <- Seurat::ScaleData(seurat_object)
+  
+  seurat_umap <- Seurat::RunUMAP(seurat_object, features = colnames(Y), seed.use = 2, metric="euclidean")
+  
+  data.frame(seurat_umap@reductions$umap@cell.embeddings)
+}
+
+Phenograph_clustering <- function(Y, k = 30){
+  library(Rphenograph)
+  Rphenograph_out <- Rphenograph(Y, k=k)
+  cl <- membership(Rphenograph_out[[2]])
+  as.numeric(cl)
 }
 
